9632c3151bee786a143d6973aa8806ba05f887ef
Oleksandr <stranger.if.2014@gmail.com>
2016-05-28 20:03:28 +0000

    added internal links in dropdown menu

--- js/script.js
+++ js/script.js
@@ -4,5 +4,25 @@ $(document).ready(function(){
     $( "ul.navbar-nav ul" ).addClass( "level_2" )
     $( "ul.level_2" ).children("li").css( "background-color", "white" ).addClass("special");
     $('tbody > tr:odd').addClass('year');
+   
+   /* Перелінковка з dropdown-menu на Nav tabs */
+   
+    $('a[href^="#travel"]').click(function(){
+        $("#travel").addClass("active");
+        $("#right_nav li:not(#travel)").removeClass('active');
+        
+    });
+    $('a[href^="#hobbi"]').click(function(){
+        $("#hobbi").addClass("active");
+        $("#right_nav li:not(#hobbi)").removeClass('active');
+    });
+    $('a[href^="#work"]').click(function(){
+        $("#work").addClass("active");
+        $("#right_nav li:not(#work)").removeClass('active');
+    });
+    $('a[href^="#links"]').click(function(){
+        $("#links").addClass("active");
+        $("#right_nav li:not(#links)").removeClass('active');
+    });
     
 });
